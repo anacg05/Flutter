1624d30d1918ec289f5db96bca4d1e1c487ad203
anacg05 <anagc09@outlook.com>
2026-03-11 16:50:40 -0300

    duas telas

--- motobile/lib/main.dart
+++ motobile/lib/main.dart
@@ -1,79 +1,35 @@
+
 import 'package:flutter/material.dart';
-import 'dart:async';
- 
+
+import 'screens/SplashScreen.dart';
+import 'screens/Onboarding1.dart';
+import 'screens/Onboarding2.dart';
+import 'screens/Login.dart';
+import 'screens/Cadastro.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu App',
+      title: 'Motobile',
       debugShowCheckedModeBanner: false,
+
       initialRoute: '/splash',
+
       routes: {
         '/splash': (context) => const SplashScreen(),
         '/onboarding1': (context) => const Onboarding1(),
+        // '/onboarding2': (context) => const Onboarding2,
+        // '/login': (context) => const Login(),
+        // '/cadastro': (context) => const Cadastro(),
       },
     );
   }
 }
- 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
- 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
- 
-class _SplashScreenState extends State<SplashScreen> {
- 
-  @override
-  void initState() {
-    super.initState();
- 
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/onboarding1');
-    });
-  }
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Image.asset(
-              'assets/images/splash.jpg',
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
- 
-class Onboarding1 extends StatelessWidget {
-  const Onboarding1({super.key});
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Onboarding'),
-      ),
-      body: const Center(
-        child: Text(
-          'Tela de Onboarding 1',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
+
